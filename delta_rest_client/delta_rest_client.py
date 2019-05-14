b4f3ac71ddd7062a6b06049f411deede8368ad4f
Saurabh Goyal <saurabh.goyal@delta.exchange>
2019-05-14 12:13:48 +0530

    Add support for passing client order id while placing order

--- delta_rest_client/delta_rest_client.py
+++ delta_rest_client/delta_rest_client.py
@@ -209,7 +209,7 @@ class DeltaRestClient:
         response = self.create_order(order)
         return response
 
-    def place_order(self, product_id, size, side, limit_price=None, time_in_force=None, order_type=OrderType.LIMIT, post_only='false'):
+    def place_order(self, product_id, size, side, limit_price=None, time_in_force=None, order_type=OrderType.LIMIT, post_only='false', client_order_id = None):
         order = {
             'product_id': product_id,
             'size': int(size),
@@ -219,9 +219,12 @@ class DeltaRestClient:
         }
         if order_type.value == 'limit_order':
             order['limit_price'] = str(limit_price)
-
+    
         if time_in_force:
             order['time_in_force'] = time_in_force.value
+        
+        if client_order_id:
+            order['client_order_id'] = client_order_id
 
         response = self.create_order(order)
         return response
